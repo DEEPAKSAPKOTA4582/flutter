63ccf59034851fdbe4be7c7ba84e47acf36b7cb5
DEEPAKSAPKOTA4582 <sapkotad570@gmail.com>
2024-04-27 20:27:46 +0545

    update in row and column screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,11 +1,6 @@
-import 'package:batch32b/screen/area_of_circle_screen.dart';
-import 'package:batch32b/screen/container_screen.dart';
-import 'package:batch32b/screen/deepak_screen.dart';
+import 'package:batch32b/screen/row_column_screen.dart';
 import 'package:flutter/material.dart';
 // import 'package:batch32b/screen/dashboard_screen.dart';
-import 'package:batch32b/screen/Container_screen.dart';
-
-import '../screen/flixible_expanded_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -14,7 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DeepakScreen(),
+      home: RowColumnScreen(),
     );
   }
 }
---
+++ lib/screen/row_column_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class RowColumnScreen extends StatelessWidget {
+  const RowColumnScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("RowAndColumn"),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+      // for row and column we create some ....
+    );
+  }
+}
